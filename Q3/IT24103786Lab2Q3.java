63b4d30a91ae439e076d15380933b06f7e2d55fc
IT24103786 <It24103786@my.sliit.lk>
2026-09-23 07:56:45 +0530

    Add files via upload

---
+++ Q3/IT24103786Lab2Q3.java
@@ -0,0 +1,9 @@
+public class IT24103786Lab2Q3 {
+    public static void main(String[] args) {
+        double sideA = 3;
+        double sideB = 4;
+        double hypotenuse = Math.sqrt((sideA * sideA) + (sideB * sideB));
+
+        System.out.println("Length of the hypotenuse: " + hypotenuse);
+    }
+}
