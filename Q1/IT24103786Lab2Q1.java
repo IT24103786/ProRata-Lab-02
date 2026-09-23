e1d94ce12c28ae444a11f07579f8a6cdd9ebf4cd
IT24103786 <It24103786@my.sliit.lk>
2026-09-23 07:56:11 +0530

    Add files via upload

---
+++ Q1/IT24103786Lab2Q1.java
@@ -0,0 +1,10 @@
+public class IT24103786Lab2Q1 {
+    public static void main(String[] args) {
+        double perimeter = 100;
+        double length = perimeter / 3.5;
+        double width = 0.75 * length;
+
+        System.out.println("Length of the fence: " + length);
+        System.out.println("Width of the fence: " + width);
+    }
+}
