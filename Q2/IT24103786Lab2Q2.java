3b13aa60da379063113ca8f8197acdcd52c77e14
IT24103786 <It24103786@my.sliit.lk>
2026-09-23 07:56:31 +0530

    Add files via upload

---
+++ Q2/IT24103786Lab2Q2.java
@@ -0,0 +1,10 @@
+public class IT24103786Lab2Q2 {
+    public static void main(String[] args) {
+        double length = 10;
+        double PI = 3.14;
+        double ropeLength = 4 * length;
+        double radius = ropeLength / (2 * PI);
+
+        System.out.println("Radius of the circular fence: " + radius);
+    }
+}
